39154755dfc74a0bef5c327f41d904ce3df0d823
KiraFlux <pret2titio1n@gmail.com>
2026-03-05 13:48:18 +0300

    Make shapshot

---
+++ src/plotter/shapshot.py
@@ -0,0 +1,20 @@
+import sys
+from pathlib import Path
+
+sys.stdout = open("out.txt", "wt", encoding='utf-8')
+
+DIR = Path(".")
+
+target_ext = (
+    "h", "cpp", "md"
+)
+
+for ext in target_ext:
+    for file in DIR.rglob(f"*.{ext}"):
+        try:
+            print(f"{file.relative_to(DIR)}")
+            print(f"```{ext}")
+            print(file.read_text(encoding='utf-8', errors='ignore').rstrip())
+            print("```\n")
+        except:
+            pass
